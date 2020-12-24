5cd582f65d91fe8a7b6129c4a0a0bd31b5eb0d2c
davimc <davimatosc@hotmail.com>
2020-12-24 11:52:39 -0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Meu primeiro código com integração contínua'));
+app.get('/', (req, res) => res.send('Testando pipeline de integração contínua'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
